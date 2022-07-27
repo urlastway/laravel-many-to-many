b67b0ee5bd6c1301512852456995273c3be89afe
urlastway <andriy.semenchenko13@gmail.com>
2022-07-27 03:15:51 +0200

    add model Tag + belongsToMany(Post, Tag)

--- app/Http/Controllers/Post.php
+++ app/Http/Controllers/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function tags(){
+        return $this->belongsToMany('App\Tags');
+    }
 }
