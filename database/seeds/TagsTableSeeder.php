4dfb84a483a70ba78b233aa78656ee36d1bb94f6
urlastway <andriy.semenchenko13@gmail.com>
2022-07-27 03:22:58 +0200

    add TagsTableSeeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['html', 'css', 'php', 'laravel', 'sql', 'js'];
+        foreach($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::of($newTag->name)->slug('-');
+            $newTag->save();
+        }
+    }
+}
