836e2e6bb81ef1e7e8f9c6db2d9b1d661ae19227
urlastway <andriy.semenchenko13@gmail.com>
2022-07-31 15:02:38 +0200

    aggiunta route API

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index(){
         
-        $posts = Post::all();
+        $posts = Post::where('published', true)->get();
 
         return response()->json($posts);
     }
